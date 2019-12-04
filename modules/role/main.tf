b2aa17958de287eaecf0428f5a57d84f0ea54bec
Claudio Iabichino <ciabichino@tierratelematics.com>
2019-12-04 17:21:21 +0100

    Upgrade to terraform 0.12

--- modules/role/main.tf
+++ modules/role/main.tf
@@ -1,27 +1,30 @@
+locals {
+
+  prefix = format("%s-%s-%s", var.project, var.environment, var.subject)
+}
+
 /*
  * Resources
  */
 
 resource "aws_iam_role" "role" {
-  name               = "${var.project}-${var.environment}-${var.subject}-role"
-  assume_role_policy = "${data.aws_iam_policy_document.assume-role-policy-document.json}"
-  path               = "${var.path}"
+  name               = format("%s-role", local.prefix)
+  assume_role_policy = data.aws_iam_policy_document.assume-role-policy-document.json
+  path               = var.path
 }
 
 data "aws_iam_policy_document" "assume-role-policy-document" {
-  "statement" {
+  
+  statement {
     effect = "Allow"
 
     principals {
-      identifiers = [
-        "${var.principals}",
-      ]
-
+      identifiers = var.principals
       type = "Service"
     }
 
     actions = [
-      "sts:AssumeRole",
+      "sts:AssumeRole"
     ]
   }
 }
@@ -31,15 +34,15 @@ data "aws_iam_policy_document" "assume-role-policy-document" {
 //
 
 data "aws_iam_policy" "managed-policy" {
-  count = "${length(var.policies)}"
-  arn   = "${element(var.policies, count.index)}"
+  count = length(var.policies)
+  arn   = element(var.policies, count.index)
 }
 
 resource "aws_iam_role_policy_attachment" "existing-role-policy-attachment" {
-  count = "${length(var.policies)}"
+  count = length(var.policies)
 
-  policy_arn = "${element(data.aws_iam_policy.managed-policy.*.arn, count.index)}"
-  role       = "${aws_iam_role.role.name}"
+  policy_arn = element(data.aws_iam_policy.managed-policy.*.arn, count.index)
+  role       = aws_iam_role.role.name
 }
 
 //
@@ -47,33 +50,29 @@ resource "aws_iam_role_policy_attachment" "existing-role-policy-attachment" {
 //
 
 resource "aws_iam_policy" "policy" {
-  count = "${length(var.actions) > 0 ? 1 : 0}"
+  count = length(var.actions) > 0 ? 1 : 0
 
-  name        = "${var.project}-${var.environment}-${var.subject}-policy"
+  name        = format("%s-policy", local.prefix)
   path        = "/"
-  description = "${var.project}-${var.environment}-${var.subject}-policy"
-  policy      = "${data.aws_iam_policy_document.policy-document.json}"
+  description = format("%s-policy", local.prefix)
+  policy      = data.aws_iam_policy_document.policy-document[0].json
 }
 
 data "aws_iam_policy_document" "policy-document" {
-  count = "${length(var.actions) > 0 ? 1 : 0}"
+  count = length(var.actions) > 0 ? 1 : 0
 
-  "statement" {
-    effect = "${var.effect}"
+  statement {
 
-    resources = [
-      "${var.resources}",
-    ]
+    effect = var.effect
+    resources = var.resources
+    actions = var.actions
 
-    actions = [
-      "${var.actions}",
-    ]
   }
 }
 
 resource "aws_iam_role_policy_attachment" "role-policy-attachment" {
-  count = "${length(var.actions) > 0 ? 1 : 0}"
+  count = length(var.actions) > 0 ? 1 : 0
 
-  policy_arn = "${aws_iam_policy.policy.arn}"
-  role       = "${aws_iam_role.role.name}"
+  policy_arn = aws_iam_policy.policy[0].arn
+  role       = aws_iam_role.role.name
 }
